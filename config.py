19bef1fc1e2d297e53cf4f21c38b12c5de292ded
Innocent me <thakareankit46@gmail.com>
2025-02-22 12:44:35 +0530

    Update config.py

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ env_vars = {
   # {chap_name} : Manga Name
   # Ex : Chapter {chap_num} {chap_name} @Manhwa_Arena
   "FNAME": "[MS] [{chap_num}] {chap_name} @Manga_Sect"  
-}
+
 
 dbname = env_vars.get('DATABASE_URL_PRIMARY') or env_vars.get('DATABASE_URL') or 'sqlite:///test.db'
 
